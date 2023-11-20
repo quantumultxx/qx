3c1e6a6790cdfef2c6e6aeae7652a4682e1f6711
quantumultxx <62304924+quantumultxx@users.noreply.github.com>
2023-11-20 15:09:39 +0800

    Delete kuwo.js

--- kuwo.js
+++
@@ -1,5 +0,0 @@
-
-
-
-
-^(?!.*img).*?(kuwo\.cn|lrts\.me)((/commercia)?/vip|/openapi)?(/enc|/v[\d]/(user(/vip\?(apiVersion|platform|op\=ui|_t)|base)|theme|sysinfo|api/pay/(user/i|payI)nfo|-album-)|/kuwopay/vip-tab/setting|/a\.p$|/music\.pay\?newver\=3$|/(EcomResource|(Mobile)?Ad)Serv(er|ice)
