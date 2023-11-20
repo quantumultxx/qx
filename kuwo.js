2354d7a3b64685c9f3ece65e064027465e21961e
quantumultxx <62304924+quantumultxx@users.noreply.github.com>
2023-11-20 14:47:00 +0800

    更新 kuwo.js

---
+++ kuwo.js
@@ -0,0 +1,6 @@
+
+#musicpay.kuwo.cn
+
+
+
+^(?!.*img).*?(kuwo\.cn|lrts\.me)((/commercia)?/vip|/openapi)?(/enc|/v[\d]/(user(/vip\?(apiVersion|platform|op\=ui|_t)|base)|theme|sysinfo|api/pay/(user/i|payI)nfo|-album-)|/kuwopay/vip-tab/setting|/a\.p$|/music\.pay\?newver\=3$|/(EcomResource|(Mobile)?Ad)Serv(er|ice)
