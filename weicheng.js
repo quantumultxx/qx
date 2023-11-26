a725f642d3f662d00404b4a68b0a37ddacc747f6
quantumultxx <62304924+quantumultxx@users.noreply.github.com>
2023-11-27 00:23:02 +0800

    Update weicheng.js

--- weicheng.js
+++ weicheng.js
@@ -7,7 +7,7 @@ hostname = api.vnce.top
 */
 
 
-let customExpiresDate = "3742732800000”; // 你想要设置的新的 expires_date_ms 变量
+let customExpiresDate = "1912348800000"; // 你想要设置的新的 expires_date_ms 变量
 
 let response = {
   "message": "success",
