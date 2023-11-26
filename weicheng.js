6c8da2cdde9417fb52e119d6d38e63ff2283ab76
quantumultxx <62304924+quantumultxx@users.noreply.github.com>
2023-11-27 00:01:28 +0800

    Update weicheng.js

--- weicheng.js
+++ weicheng.js
@@ -1,5 +1,6 @@
 /*
 [rewrite_local]
+^http:\/\/api\.vnce\.top\/wenetproductid url script-response-body https://raw.githubusercontent.com/quantumultxx/qx/main/weicheng.js
 ^http:\/\/api\.vnce\.top\/wenetverifyticket url script-response-body https://raw.githubusercontent.com/quantumultxx/qx/main/weicheng.js
 
 [mitm]
@@ -8,5 +9,6 @@ hostname = api.vnce.top
 
 
 var body = $response.body;
+obj.productid = "wenet_fee_730day";
 obj.expires_date_ms = "1743258555";
 $done({ body: JSON.stringify(obj) });
