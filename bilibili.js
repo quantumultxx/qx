1afbe6cb32fef66e3537adbb3aa3cdf442865c1f
quantumultxx <62304924+quantumultxx@users.noreply.github.com>
2023-11-26 16:44:29 +0800

    Update bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -1,10 +1,11 @@
 /*
-新版需要自己抓包找域名
+
+新版需要自己抓包找域名
 
 [rewrite_local]
 
 #哔哩哔哩解锁大会员
-^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header blbl.js
+^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header https://raw.githubusercontent.com/quantumultxx/qx/main/bilibili.js
 
 [mitm]
 hostname = *.biliapi.*, *.bilibili.*
