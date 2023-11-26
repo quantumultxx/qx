ad1debe718f2e166645e959a2d14fd5a272b3b46
quantumultxx <62304924+quantumultxx@users.noreply.github.com>
2023-11-26 23:38:37 +0800

    Update weicheng.js

--- weicheng.js
+++ weicheng.js
@@ -1,16 +1,15 @@
 /*
 [rewrite_local]
-^http:\/\/api\.vnce\.top\/wenetverifyticket url 
-script-response-body https://raw.githubusercontent.com/quantumultxx/qx/main/weicheng.js
+^http:\/\/api\.vnce\.top\/wenetverifyticket url script-response-body https://raw.githubusercontent.com/quantumultxx/qx/main/weicheng.js
 
 [mitm]
 hostname = api.vnce.top
 */
 
 
-let obj = JSON.parse($response.body);
+var body = $response.body;
 obj.state = 200;
-obj.message = "正常";
-obj.expires_date_ms = "1743258555";
+obj.message = 正常;
+obj.expires_date_ms = 1743258555;
 obj.code = 200;
 $done({ body: JSON.stringify(obj) });
