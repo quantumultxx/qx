04d7e352699961a8e7a20a58be832081029912ab
quantumultxx <62304924+quantumultxx@users.noreply.github.com>
2023-11-27 01:28:04 +0800

    Update weicheng.js

--- weicheng.js
+++ weicheng.js
@@ -1,35 +1,21 @@
 /*
 [rewrite_local]
-^http:\/\/api\.vnce\.top\/wenetproductid url script-response-body https://raw.githubusercontent.com/quantumultxx/qx/main/weicheng.js
 ^http:\/\/api\.vnce\.top\/wenetverifyticket url script-response-body https://raw.githubusercontent.com/quantumultxx/qx/main/weicheng.js
 
 [mitm]
 hostname = api.vnce.top
 */
 
+let customExpiresDate = "1912348800000"; // 你想要设置的新的 expires_date_ms 变量
 
-let response1 = {
+let response = {
   "message": "success",
   "data": {
     "state": 200,
     "message": "正常",
-    "expires_date_ms": "1711722555000"
+    "expires_date_ms": customExpiresDate
   },
   "code": 200
 };
 
-let response2 = {
-  "message": "成功",
-  "data": {
-    "productid": "wenet_fee_365day"
-  },
-  "code": "200"
-};
-
-let expires_date_ms_variable = "1901024955000"; // 你想要的新 expires_date_ms 值
-let productid_variable = "wenet_free_2557day"; // 你想要的新 productid 值
-
-response1.data.expires_date_ms = expires_date_ms_variable;
-response2.data.productid = productid_variable;
-
-$done({ body: JSON.stringify(response1) + '\n' + JSON.stringify(response2) });
+$done({ body: JSON.stringify(response) });
