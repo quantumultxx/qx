baa007cf7a8a2750256a3a21056c2ad12e18499f
quantumultxx <62304924+quantumultxx@users.noreply.github.com>
2023-11-27 12:13:59 +0800

    Update weicheng.js

--- weicheng.js
+++ weicheng.js
@@ -7,7 +7,6 @@
 hostname = api.vnce.top
 */
 
-
 let response1 = {
   "message": "success",
   "data": {
@@ -26,10 +25,14 @@ let response2 = {
   "code": "200"
 };
 
-let expires_date_ms_variable = "1912348800000000"; // 你想要的新 expires_date_ms 值
+let expires_date_ms_variable =  "1912348800000000"; // 你想要的新 expires_date_ms 值
 let productid_variable = "wenet_fee_2557day"; // 你想要的新 productid 值
 
 response1.data.expires_date_ms = expires_date_ms_variable;
 response2.data.productid = productid_variable;
 
-$done({ body: JSON.stringify([response1, response2]) });
+// 先执行 response1
+$done({ body: JSON.stringify(response1) });
+
+// 然后执行 response2
+$done({ body: JSON.stringify(response2) });
